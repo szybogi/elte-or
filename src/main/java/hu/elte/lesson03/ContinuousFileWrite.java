98dd38366e9486abe45af7ad1bacec61c6b255b9
szybogi <szy.bogi@gmail.com>
2019-03-06 19:11:21 +0100

    init

--- src/main/java/hu/elte/lesson03/ContinuousFileWrite.java
+++ src/main/java/hu/elte/lesson03/ContinuousFileWrite.java
@@ -1,10 +1,26 @@
 package hu.elte.lesson03;
 
 
-public class ContinuousFileWrite
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class ContinuousFileWrite {
+    public static void main(String[] args) {
+        File file = new File("./src/main/resources/hu/elte/lesson03/output.txt");
+        try (
+                PrintWriter pw = new PrintWriter(file);
+                Scanner scanner = new Scanner(System.in);
+        ) {
+            String line = scanner.nextLine();
+            while (!line.equals("end")) {
+                pw.println(line);
+                pw.flush();
+                line = scanner.nextLine();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
 }
