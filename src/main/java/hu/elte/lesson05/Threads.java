f627db56407787c28db76a26bbd18dfccc433e79
Szy Boglárka <szy.bogi@gmail.com>
2019-03-14 10:55:11 +0100

    lesson5

--- src/main/java/hu/elte/lesson05/Threads.java
+++ src/main/java/hu/elte/lesson05/Threads.java
@@ -3,12 +3,13 @@ package hu.elte.lesson05;
 public class Threads {
 
     public static void main(String[] args) {
-        Thread t1 = new Thread(() -> characterPrinter("Hello "));
-        t1.start();
-        new Thread(() -> characterPrinter("vilag")).start();
-
         try {
+            Thread t1 = new Thread(() -> characterPrinter("Hello "));
+            t1.start();
             t1.join();
+            Thread t2 = new Thread(() -> characterPrinter("vilag"));
+            t2.start();
+            t2.join();
         } catch (InterruptedException e) {
         }
         characterPrinter("!");
